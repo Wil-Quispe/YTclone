8b38dcdf7eebee163eaf3f161d9033cdd7da4cfe
@Wil <emma.wilinston@gmail.com>
2021-05-27 15:46:11 -0500

    finaliza el clon

---
+++ hooks/useFetch.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+interface Search {
+  search: string
+  maxResults?: number
+}
+
+const Fetch = () => {
+  const [PlayList, setPlayList] = useState()
+  useEffect(() => {
+    try {
+      const getPlayList = async (): Promise<void> => {
+        const res = await fetch(
+          'https://www.googleapis.com/youtube/v3/playlists?part=snippet&key=AIzaSyCmG8jzvCF6_3kPYRcGzSF9b2L-LF6j68s&channelId=UC2xLpPmnDyAxvHlOTOdt_Ug&maxResults=10'
+        )
+        const playlists = await res.json()
+        setPlayList(playlists.items)
+      }
+      getPlayList()
+    } catch (e) {
+      return e
+    }
+  }, [])
+
+  const doFetch = async ({ search, maxResults }: Search): Promise<void> => {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCmG8jzvCF6_3kPYRcGzSF9b2L-LF6j68s&maxResults=${
+          maxResults || 10
+        }&type=video&q=${search}`
+      )
+      const data = await res.json()
+      return data
+    } catch (e) {
+      return e
+    }
+  }
+
+  return [doFetch, PlayList]
+}
+
+export default Fetch
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,89 @@
-const IndexPage = () => (
-  <h1>hola bros</h1>
+import { ChangeEvent, useState } from 'react'
+import useFetch from '../hooks/useFetch'
+
+interface SearchInterface {
+  search: string
+  maxResults?: number
+}
+
+const style: React.CSSProperties = { padding: '1em', textAlign: 'center' }
+
+const IndexPage: React.FC = (): JSX.Element => {
+  const [doFetch, PlayList] = useFetch()
+  const [Videos, setVideos] = useState()
+
+  const [Search, setSearch] = useState<SearchInterface>({
+    search: '',
+    maxResults: 0,
+  })
+
+  const search = async (e: any): Promise<void> => {
+    e.preventDefault()
+    const result = await doFetch(Search)
+    setVideos(result.items)
+  }
+
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target
+    setSearch({ ...Search, [name]: value })
+  }
+
   // yarn create next-app --example with-typescript
-)
+
+  return (
+    <>
+      <h1>hola bros YTclone</h1>
+
+      <form onSubmit={search}>
+        <label>
+          <input
+            type='text'
+            placeholder='buscar'
+            name='search'
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <input
+            type='number'
+            placeholder='max resultado'
+            name='maxResults'
+            onChange={handleChange}
+          />
+        </label>
+        <button type='submit'>Buscar</button>
+      </form>
+
+      <div style={{ ...style }}>
+        {Videos &&
+          Videos.map((v: any, i: number) => (
+            <iframe
+              style={{ padding: '.8em .5em' }}
+              key={i}
+              src={`https://www.youtube.com/embed/${v.id.videoId}`}
+              frameBorder='0'
+              allowFullScreen
+            ></iframe>
+          ))}
+      </div>
+
+      <div style={{ ...style }}>
+        {PlayList ? (
+          PlayList.map((p: any, i: number) => (
+            <iframe
+              style={{ padding: '.8em .5em' }}
+              key={i}
+              src={`https://www.youtube.com/embed/videoseries?list=${p.id}`}
+              frameBorder='0'
+              allowFullScreen
+            ></iframe>
+          ))
+        ) : (
+          <h3>Cargando PlayList ...</h3>
+        )}
+      </div>
+    </>
+  )
+}
 
 export default IndexPage
